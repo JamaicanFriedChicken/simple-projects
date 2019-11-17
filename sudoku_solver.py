21b6cbfd1fa1a72ba21d0f789c3c85c690d56577
JamaicanFriedChicken <mikey_lue777@hotmail.com>
2019-11-17 22:38:36 +0800

    solving sudoku puzzle

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -91,6 +91,16 @@ def some(seq):
     return False
 
 
+def display(values):
+    "Display these values as a 2D grid."
+    width = 1 + max(len(values[s]) for s in squares)
+    line = '+'.join(['-' * (width*3)] * 3)
+    for r in rows:
+        print(''.join(values[r + c].center(width) + ('|' if c in '36' else '') for c in cols))
+        if r in 'CF':
+            print(line)
+    print()
+
 grid = """
 4 . . |. . . |8 . 5 
 . 3 . |. . . |. . . 
@@ -104,3 +114,5 @@ grid = """
 5 . . |2 . . |. . . 
 1 . 4 |. . . |. . . 
 """
+
+display(parse_grid(grid))
