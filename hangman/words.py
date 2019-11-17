f4c22526e964d991504c12a7fecf301c0d93c4bc
JamaicanFriedChicken <mikey_lue777@hotmail.com>
2019-11-17 22:40:04 +0800

    used reservoir sampling to be more memory efficient

--- hangman/words.py
+++ hangman/words.py
@@ -1,23 +1,21 @@
 import random
 
-wordList = 'wordlist.txt'
+WORDLIST = 'wordlist.txt'
 
 def get_random_word(min_word_length):
     """ Chooses a random word from the word list using no extra memory.
     """
-    words = []
-    with open(wordList, 'r') as file:
+    num_words_processed = 0
+    current_word = None
+    with open(WORDLIST, 'r') as file:
         for word in file:
-
-            # skips word if contains parentheses
-            if '(' or ')' in word:
+            if '(' in word or ')' in word:
                 continue
             word = word.strip().lower()
-
-            # skip word if it's too short
             if len(word) < min_word_length:
                 continue
-
-            words.append(word)
-    return random.choice(words)
+            num_words_processed += 1
+            if random.randint(1, num_words_processed) == 1:
+                current_word = word
+    return current_word
 
