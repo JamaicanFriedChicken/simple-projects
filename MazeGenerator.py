a266a583f30f7ef81de60f6211ab1a18eb152933
JamaicanFriedChicken <mikey_lue777@hotmail.com>
2019-11-22 13:56:58 +0800

    added documentation

--- MazeGenerator.py
+++ MazeGenerator.py
@@ -2,11 +2,23 @@ from random import shuffle, randrange
 
 
 def make_maze(w, h):
+    """
+    Generates a random maze using Depth first search algorithm.
+    w : width of the maze
+    h : height of the maze
+
+    Set the parameters as you would like for the size of the generated maze.
+    """
     visualize_grid = [[0] * w + [1] for _ in range(h)] + [[1] * (w + 1)]
     vertical = [["|  "] * w + ['|'] for _ in range(h)] + [[]]
     horizontal = [["+--"] * w + ['+'] for _ in range(h + 1)]
 
     def walk(x ,y):
+        """
+            Walk function generates the maze's interior design through random generations and spacing between
+        each partitions. The maze will be different each time due to the randrange and shuffle function
+        imported from random library.
+        """
         visualize_grid[y][x] = 1
 
         d = [(x - 1, y), (x, y +1), (x + 1, y), (x, y - 1)]
@@ -19,9 +31,11 @@ def make_maze(w, h):
 
     walk(randrange(w), randrange(h))
 
+    # assembles the grid together, whereby a consists of '+--' and 'x ', however b
+    # consists of partitions followed by empty spaces in between the partitions.
     s = ""
     for (a,b) in zip(horizontal, vertical):
-        s += ''.join(a + ['\n'] + b + ['\n'])
+        s += ''.join( a+ ['\n'] + b + ['\n'])
     return s
 
 
