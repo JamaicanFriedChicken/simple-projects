61cc28473f441e54e32e9e1d0430a7a35de2fda4
JamaicanFriedChicken <mikey_lue777@hotmail.com>
2019-12-03 11:07:18 +0800

    Added first javascript project

---
+++ javascript/AgeInDays/home.js
@@ -0,0 +1,13 @@
+// Age in days
+
+function age() {
+    var birthYear = prompt("What year were you born in?");
+    var ageInDays = (2019 - birthYear) * 365;
+    var h1 = document.createElement('h1');
+    var textAnswer = document.createTextNode('You are ' + ageInDays + ' days old');
+    h1.setAttribute('id', 'ageInDays');
+    h1.appendChild(textAnswer);
+    document.getElementById('flex-box-results').appendChild(h1);
+    console.log(ageInDays);
+}
+
