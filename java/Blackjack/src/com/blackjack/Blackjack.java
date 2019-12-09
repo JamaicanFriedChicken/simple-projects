c53b56f8a7b73e19433239fde04536a915215cd4
JamaicanFriedChicken <mikey_lue777@hotmail.com>
2019-12-09 23:21:27 +0800

    finished implementing blackjack game

--- java/Blackjack/src/com/blackjack/Blackjack.java
+++ java/Blackjack/src/com/blackjack/Blackjack.java
@@ -1,5 +1,7 @@
 package com.blackjack;
 
+import java.util.Scanner;
+
 class Blackjack {
 
     public static void main(String[] args) {
@@ -10,7 +12,115 @@ class Blackjack {
         // Create our playing deck
         Deck playingDeck = new Deck();
         playingDeck.createFullDeck();
+        playingDeck.shuffle();
 
-        System.out.println(playingDeck);
+        // Creating a deck for the player and dealer
+        Deck playerDeck = new Deck();
+        Deck dealerDeck = new Deck();
+
+        double playerMoney = 100.00;
+
+        Scanner userInput = new Scanner(System.in);
+
+        // Game Loop
+        while (playerMoney > 0) {
+            // Plays on if player still has money
+            // Take the players bet
+            System.out.println("You have $" + playerMoney + ", how much would you like to bet?");
+            double playerBet = userInput.nextDouble();
+            if (playerBet > playerMoney) {
+                System.out.println("You can't bet more money than what you have!");
+                break;
+            }
+
+            boolean endRound = false;
+
+            // Start dealing
+            // Player gets two cards
+            playerDeck.draw(playingDeck);
+            playerDeck.draw(playingDeck);
+
+            // Dealer gets two cards
+            dealerDeck.draw(playingDeck);
+            dealerDeck.draw(playingDeck);
+
+            // Loop for hitting (drawing cards from dealer)
+            while (true) {
+                System.out.println("Your hand:");
+                System.out.println(playerDeck.toString() + "\n");
+                System.out.println("Your hand is:" + playerDeck.cardsValue());
+
+                // Display Dealer hand
+                System.out.println("Dealer Hand: " + dealerDeck.getCard(0).toString() + " and [HIDDEN]");
+
+                // What does the player want to do?
+                System.out.println("Would you like to (1)Hit or (2)Stand?");
+                int response = userInput.nextInt();
+
+                // They hit
+                if (response == 1) {
+                    playerDeck.draw(playingDeck);
+                    System.out.println("You drew a " + playerDeck.getCard(playerDeck.deckSize() -1));
+                    // Loses if over cards value over 21
+                    if (playerDeck.cardsValue() > 21) {
+                        System.out.println("You lose! Currently valued at: " + playerDeck.cardsValue());
+                        playerMoney -= playerBet;
+                        endRound = true;
+                        break;
+                    }
+                }
+
+                if (response == 2) {
+                    break;
+                }
+            }
+
+            // Reveal Dealer Cards
+            System.out.println("Dealer Cards: " + dealerDeck.toString());
+            // Checks to see if dealer's hand is greater than player's hand
+            if (dealerDeck.cardsValue() > playerDeck.cardsValue() && endRound == false) {
+                System.out.println("Dealer beats you!");
+                playerMoney -= playerBet;
+                endRound = true;
+            }
+
+            // Dealers draws at 16, stand at 17
+            while (dealerDeck.cardsValue() < 17 && endRound == false) {
+                dealerDeck.draw(playingDeck);
+                System.out.println("Dealer draws: " + dealerDeck.getCard(dealerDeck.deckSize()-1).toString());
+            }
+
+            // Display total value of cards in Dealer's hand
+            System.out.println("Dealer's hand is valued at: " + dealerDeck.cardsValue());
+
+            // Determine if dealer loses
+            if ((dealerDeck.cardsValue() > 21) && endRound == false) {
+                System.out.println(("Dealer loses! You win!"));
+                playerMoney += playerBet;
+                endRound = true;
+            }
+
+            // Determine if push (means a tie in blackjack)
+            if ((playerDeck.cardsValue() == dealerDeck.cardsValue()) && endRound == false) {
+                System.out.println("Push");
+                endRound = true;
+            }
+
+            if ((playerDeck.cardsValue() > dealerDeck.cardsValue()) && endRound == false) {
+                System.out.println("You win!");
+                playerMoney += playerBet;
+                endRound = true;
+            } else if (endRound == false) {
+                System.out.println("You lose the game.");
+                playerMoney -= playerBet;
+                endRound = true;
+            }
+
+            playerDeck.moveAllToDeck(playingDeck);
+            dealerDeck.moveAllToDeck(playingDeck);
+            System.out.println("End of hand.");
+        }
+
+        System.out.println("Gamer Over! You are out of money.");
     }
 }
--- java/Blackjack/src/com/blackjack/Deck.java
+++ java/Blackjack/src/com/blackjack/Deck.java
@@ -1,6 +1,7 @@
 package com.blackjack;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Deck {
 
@@ -22,13 +23,103 @@ public class Deck {
         }
     }
 
+    public void shuffle() {
+        ArrayList<Card> temporaryDeck = new ArrayList<Card>();
+        // Use random function to generate random deck
+        Random random = new Random();
+        int randomCardIndex = 0;
+        int originalSize = this.cards.size();
+        for (int i = 0; i < originalSize; i++) {
+            // Generate random index, to get a random number formula: rand.nextInt((max - min) + 1) + min;
+            // range is between 0 and 51 (total number of cards in deck is 52)
+            // below the formula is this.cards.size() - 1 => 52 - 1 = 51 for this ^
+            randomCardIndex = random.nextInt((this.cards.size() - 1 - 0) + 1) + 0;
+            temporaryDeck.add(this.cards.get(randomCardIndex));
+
+            // remove card from original deck
+            this.cards.remove(randomCardIndex);
+        }
+
+        this.cards = temporaryDeck;
+    }
+
     public String toString() {
         String cardListOutput = "";
-        int i = 0;
         for (Card aCard : this.cards) {
-            cardListOutput += "\n" + i + "-" + aCard.toString();
-            i++;
+            cardListOutput += "\n " + aCard.toString();
         }
         return cardListOutput;
     }
+
+    public void removeCard(int i) {
+        this.cards.remove(i);
+    }
+
+    public Card getCard(int i) {
+        return this.cards.get(i);
+    }
+
+    public void addCard(Card addCard) {
+         this.cards.add(addCard);
+    }
+
+    // Draws from the deck
+    public void draw(Deck comingFrom) {
+        this.cards.add(comingFrom.getCard(0));
+        comingFrom.removeCard(0);
+    }
+
+    public int deckSize() {
+        return this.cards.size();
+    }
+
+    public void moveAllToDeck(Deck moveTo) {
+        int thisDeckSize = this.cards.size();
+
+        // Put cards into moveTo deck
+        for (int i = 0; i < thisDeckSize; i++) {
+            moveTo.addCard(this.getCard(i));
+        }
+
+        for (int i = 0; i < thisDeckSize; i++) {
+            this.removeCard(0);
+        }
+    }
+
+    // Return the total value of cards in deck
+    public int cardsValue() {
+        int totalValue = 0;
+        int aces = 0;
+
+        for (Card aCard : this.cards) {
+            switch(aCard.getValue()) {
+                case ACE: totalValue += 1; break;
+                case TWO: totalValue += 2; break;
+                case THREE: totalValue += 3; break;
+                case FOUR: totalValue += 4; break;
+                case FIVE: totalValue += 5; break;
+                case SIX: totalValue += 6; break;
+                case SEVEN: totalValue += 7; break;
+                case EIGHT: totalValue += 8; break;
+                case NINE: totalValue += 9; break;
+                case TEN: totalValue += 10; break;
+                case JACK: totalValue += 11; break;
+                case QUEEN: totalValue += 12; break;
+                case KING: totalValue += 13; break;
+            }
+        }
+
+        for (int i = 0; i < aces; i++){
+            // condition for switching the Aces value. Either 11 or 1.
+            if (totalValue > 10) {
+                totalValue += 1;
+            } else {
+                totalValue += 11;
+            }
+        }
+
+        return totalValue;
+    }
+
+
 }
